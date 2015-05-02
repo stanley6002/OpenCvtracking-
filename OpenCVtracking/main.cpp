b293f413012feb3c0a2f581c7a82e451f876caf6
Chih-Hsiang Chang <admin@example.com>
2015-05-01 20:52:13 -0500

    Initial Commit

---
+++ OpenCVtracking/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCVtracking
+//
+//  Created by C-HChang on 5/1/15.
+//  Copyright (c) 2015 C-HChang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
